deb3c0de9ede1f1aed42be65fa175bd81c6b7f96
nabihah035 <nabihahzulkefli1@gmail.com>
2025-07-17 23:14:10 +0800

    Update and rename AuthDAO to AuthDAO.java

--- Database/AuthDAO.java
+++ Database/AuthDAO.java
@@ -1,9 +1,11 @@
 package database;
 
+import model.Customer;
 import model.Rider;
 import model.Vendor;
 
 public interface AuthDAO {
+    Customer validateLogin(String email, String password);
     Rider validateRider(String email, String password);
     Vendor validateVendor(String email, String password);
 }
